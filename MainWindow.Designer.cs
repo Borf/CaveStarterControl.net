2b6276352886c2a59be5c49b9fc41920939fcf07
Johan Talboom <borfje@gmail.com>
2016-03-30 16:26:07 +0200

    Initial commit

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace CaveStarterControl.net
+{
+	partial class MainWindow
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.txtArguments = new System.Windows.Forms.TextBox();
+			this.txtCommand = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtWorkingDirectory = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.button2 = new System.Windows.Forms.Button();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.listView2 = new System.Windows.Forms.ListView();
+			this.panel1.SuspendLayout();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Controls.Add(this.txtArguments);
+			this.panel1.Controls.Add(this.txtCommand);
+			this.panel1.Controls.Add(this.label4);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.txtWorkingDirectory);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(3, 3);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(668, 150);
+			this.panel1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Location = new System.Drawing.Point(573, 112);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 7;
+			this.button1.Text = "Start";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// txtArguments
+			// 
+			this.txtArguments.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtArguments.Location = new System.Drawing.Point(123, 86);
+			this.txtArguments.Name = "txtArguments";
+			this.txtArguments.Size = new System.Drawing.Size(525, 20);
+			this.txtArguments.TabIndex = 6;
+			// 
+			// txtCommand
+			// 
+			this.txtCommand.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtCommand.Location = new System.Drawing.Point(123, 60);
+			this.txtCommand.Name = "txtCommand";
+			this.txtCommand.Size = new System.Drawing.Size(525, 20);
+			this.txtCommand.TabIndex = 5;
+			this.txtCommand.Text = "JohanDemo_release.exe";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(14, 89);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(57, 13);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "Arguments";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(14, 63);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(54, 13);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Command";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(14, 38);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(92, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Working Directory";
+			// 
+			// txtWorkingDirectory
+			// 
+			this.txtWorkingDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtWorkingDirectory.Location = new System.Drawing.Point(123, 35);
+			this.txtWorkingDirectory.Name = "txtWorkingDirectory";
+			this.txtWorkingDirectory.Size = new System.Drawing.Size(525, 20);
+			this.txtWorkingDirectory.TabIndex = 1;
+			this.txtWorkingDirectory.Text = "C:\\VrCave\\Runtime";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(14, 11);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(78, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Manual Control";
+			// 
+			// listView1
+			// 
+			this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listView1.FullRowSelect = true;
+			this.listView1.Location = new System.Drawing.Point(3, 159);
+			this.listView1.MultiSelect = false;
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(678, 322);
+			this.listView1.TabIndex = 1;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			this.listView1.ItemActivate += new System.EventHandler(this.listView1_ItemActivate);
+			// 
+			// button2
+			// 
+			this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button2.Location = new System.Drawing.Point(603, 1);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 8;
+			this.button2.Text = "Stop";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Location = new System.Drawing.Point(3, 3);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(694, 513);
+			this.tabControl1.TabIndex = 2;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.panel1);
+			this.tabPage1.Controls.Add(this.listView1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(686, 487);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Manual Control";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.listView2);
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(686, 487);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Visual Control";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// listView2
+			// 
+			this.listView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listView2.Location = new System.Drawing.Point(-4, 0);
+			this.listView2.Name = "listView2";
+			this.listView2.Size = new System.Drawing.Size(690, 491);
+			this.listView2.TabIndex = 0;
+			this.listView2.UseCompatibleStateImageBehavior = false;
+			this.listView2.ItemActivate += new System.EventHandler(this.listView2_ItemActivate);
+			// 
+			// MainWindow
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(690, 513);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.tabControl1);
+			this.Name = "MainWindow";
+			this.Text = "Cave Starter";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.Load += new System.EventHandler(this.MainWindow_Load);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage2.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.TextBox txtArguments;
+		private System.Windows.Forms.TextBox txtCommand;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtWorkingDirectory;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.ListView listView2;
+	}
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaveStarterControl.net
+{
+	public partial class MainWindow : Form
+	{
+		public const string historyFile = "data/CaveStarter.net/history.txt";
+		public const string simpleFile = "data/CaveStarter.net/simple.txt";
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void MainWindow_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				string[] lines = File.ReadAllLines(historyFile);
+				for (int i = 0; i < lines.Length; i += 4)
+				{
+					listView1.Items.Add(new ListViewItem(new string[] { lines[i], lines[i+1], lines[i + 2], lines[i + 3] }));
+				}
+			}
+			catch (FileNotFoundException)
+			{
+			}
+			listView1.Columns.Add("Last Run", 100, HorizontalAlignment.Left);
+			listView1.Columns.Add("Command", 200, HorizontalAlignment.Left);
+			listView1.Columns.Add("Arguments", 200, HorizontalAlignment.Left);
+			listView1.Columns.Add("Directory", 150, HorizontalAlignment.Left);
+
+
+
+			try
+			{
+				string[] lines = File.ReadAllLines(simpleFile);
+
+				ImageList ilist = new ImageList();
+				ilist.ImageSize = new Size(256, 256);
+				ilist.ColorDepth = ColorDepth.Depth24Bit;
+				this.listView2.LargeImageList = ilist;
+				for (int i = 0; i < lines.Length; i+=6 )
+				{
+					Bitmap b = new Bitmap("data/CaveStarter.net/" + lines[i+1]);
+					ilist.Images.Add(b);
+					ListViewItem lvi = new ListViewItem(lines[i]);
+					lvi.ImageIndex = i/6;
+					lvi.SubItems.Add(lines[i + 2]);
+					lvi.SubItems.Add(lines[i + 3]);
+					lvi.SubItems.Add(lines[i + 4]);
+					listView2.Items.Add(lvi);
+				}
+				listView2.Columns.Add("Last Run", 100, HorizontalAlignment.Left);
+				listView2.Columns.Add("Command", 200, HorizontalAlignment.Left);
+				listView2.Columns.Add("Arguments", 200, HorizontalAlignment.Left);
+				listView2.Columns.Add("Directory", 150, HorizontalAlignment.Left);
+			}
+			catch (Exception)
+			{
+			}
+
+
+			tabControl1.SelectedTab = tabPage2;
+
+
+		}
+
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string command = txtCommand.Text;
+			string directory = txtWorkingDirectory.Text;
+			string arguments = txtArguments.Text;
+
+			bool found = false;
+			foreach (ListViewItem item in listView1.Items)
+			{
+				if (item.SubItems[1].Text == command &&
+				    item.SubItems[2].Text == arguments &&
+				    item.SubItems[3].Text == directory)
+				{
+					found = true;
+					item.SubItems[0].Text = DateTime.Now.ToString();
+				}
+			}
+			if(!found)
+				listView1.Items.Add(new ListViewItem(new string[] { DateTime.Now.ToString(), command, arguments, directory }));
+			saveListView();
+
+			string cmd = "simplestart ";
+			cmd += "\"" + directory + "\" ";
+			cmd += "\"" + command + "\" ";
+			cmd += "\"" + arguments + "\"\r\n";
+
+			sendCommand(cmd);
+		}
+
+		private void sendCommand(string cmd)
+		{
+			Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+			socket.Connect("localhost", 9999);
+
+			string s = myRecv(socket);
+			if (s != "hello")
+			{
+				MessageBox.Show(this, "Error: the service running is not a cavestarter server");
+				socket.Close();
+				return;
+			}
+
+			mySend(cmd, socket);
+			socket.Close();
+		}
+
+		string myRecv(Socket socket)
+		{
+			string ret = "";
+			while (ret.IndexOf("\r\n") == -1)
+			{
+				byte[] buffer = new byte[1024];
+				int rc = socket.Receive(buffer, 0, 1024, SocketFlags.None);
+				ret += System.Text.Encoding.ASCII.GetString(buffer, 0, rc);
+			}
+			ret = ret.Trim();
+			return ret;
+		}
+		void mySend(string command, Socket socket)
+		{
+			byte[] data = System.Text.Encoding.ASCII.GetBytes(command);
+			int sent = 0;
+			while (sent < command.Length)
+			{
+				int rc = socket.Send(data, sent, command.Length - sent, SocketFlags.None);
+				sent += rc;
+			}
+		}
+
+
+		void saveListView()
+		{
+			string data = "";
+			foreach (ListViewItem item in listView1.Items)
+			{
+				foreach (ListViewItem.ListViewSubItem subItem in item.SubItems)
+					data += subItem.Text + "\r\n";
+			}
+			File.WriteAllText(historyFile,data);
+			
+		}
+
+		private void listView1_ItemActivate(object sender, EventArgs e)
+		{
+			txtCommand.Text = listView1.SelectedItems[0].SubItems[1].Text;
+			txtArguments.Text = listView1.SelectedItems[0].SubItems[2].Text;
+			txtWorkingDirectory.Text = listView1.SelectedItems[0].SubItems[3].Text;
+			button1_Click(sender, e);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			sendCommand("stopvr\r\n");
+		}
+
+		private void listView2_ItemActivate(object sender, EventArgs e)
+		{
+			txtCommand.Text = listView2.SelectedItems[0].SubItems[1].Text;
+			txtArguments.Text = listView2.SelectedItems[0].SubItems[2].Text;
+			txtWorkingDirectory.Text = listView2.SelectedItems[0].SubItems[3].Text;
+			button1_Click(sender, e);
+		}
+	}
+}
